ceb30f5141f979efeeb3d94cd631d01517e0075b
MadYoung <1628933578@qq.com>
2019-06-01 19:58:24 +0800

    index对接

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,18 +1,30 @@
 <template>
-	<view class="index">
-		<block v-for="item in list" :key="item.img_src">
-			<view class="card" @click="goDetail(item)">
-				<image class="card-img" :src="item.img_src" mode="aspectFill"></image>
-				<text class="card-num-view">{{item.img_num}}P</text>
-				<view class="card-bottm row">
-					<view class="car-title-view row">
-						<text class="card-title">{{item.title}}</text>
+	<view class="container">
+		<div class="swiperBox">
+			<view class="uni-padding-wrap">
+				<view class="page-section swiper">
+					<view class="page-section-spacing">
+						<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+							<swiper-item>
+								<view class="swiper-item uni-bg-red">
+									<img src="../../static/pg1.jpg" alt="">
+								</view>
+							</swiper-item>
+							<swiper-item>
+								<view class="swiper-item uni-bg-green">
+									<img src="../../static/pg2.jpg" alt="">
+								</view>
+							</swiper-item>
+							<swiper-item>
+								<view class="swiper-item uni-bg-blue">
+									<img src="../../static/pg3.jpg" alt="">
+								</view>
+							</swiper-item>
+						</swiper>
 					</view>
-					<view @click.stop="share(item)" class="card-share-view"></view>
 				</view>
 			</view>
-		</block>
-		<text class="loadMore">加载中...</text>
+		</div>
 	</view>
 </template>
 
@@ -20,134 +32,43 @@
 	export default {
 		data() {
 			return {
-				refreshing: false,
-				providerList: [],
-				list: [],
-				fetchPageNum: 1
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
 			}
 		},
 		onLoad() {
-			this.getData();
-			uni.getProvider({
-				service: 'share',
-				success: (e) => {
-					let data = []
-					for (let i = 0; i < e.provider.length; i++) {
-						switch (e.provider[i]) {
-							case 'weixin':
-								data.push({
-									name: '分享到微信好友',
-									id: 'weixin'
-								})
-								data.push({
-									name: '分享到微信朋友圈',
-									id: 'weixin',
-									type: 'WXSenceTimeline'
-								})
-								break;
-							case 'qq':
-								data.push({
-									name: '分享到QQ',
-									id: 'qq'
-								})
-								break;
-							default:
-								break;
-						}
-					}
-					this.providerList = data;
-				},
-				fail: (e) => {
-					console.log('获取分享通道失败', e);
-				}
-			});
+			this.getDev()
 		},
-		onReachBottom() {
-			console.log('滑动到页面底部')
-			this.getData();
-		},
-		onPullDownRefresh() {
-			console.log('下拉刷新');
-			this.refreshing = true;
-			this.getData();
-		},
-		methods: {
-			getData() {
+		methods:{
+			getDev(){
 				uni.request({
-					url: this.$serverUrl + '/api/picture/posts.php?page=' + (this.refreshing ? 1 : this.fetchPageNum) +
-						'&per_page=5',
-					success: (ret) => {
-						console.log('data', ret);
-						if (ret.statusCode !== 200) {
-							console.log('失败!');
-						} else {
-							if (this.refreshing && ret.data[0].id === this.list[0].id) {
-								uni.showToast({
-									title: '已经最新',
-									icon: 'none',
-								})
-								this.refreshing = false;
-								uni.stopPullDownRefresh();
-								return;
-							}
-							if (this.refreshing) {
-								this.refreshing = false;
-								uni.stopPullDownRefresh()
-								this.list = ret.data;
-								this.fetchPageNum = 2;
-							} else {
-								this.list = this.list.concat(ret.data);
-								this.fetchPageNum += 1;
-							}
-						}
+					url: this.$serverUrl + '/mobile/index/index', //仅为示例，并非真实接口地址。
+					method: 'GET',
+					success: (res) => {
+						console.log(res.data);
 					}
 				});
-			},
-			goDetail(e) {
-				uni.navigateTo({
-					url: '../detail/detail?data=' + encodeURIComponent(JSON.stringify(e))
-				})
-			},
-			share(e) {
-				if (this.providerList.length === 0) {
-					uni.showModal({
-						title: '当前环境无分享渠道!',
-						showCancel: false
-					})
-					return;
-				}
-				let itemList = this.providerList.map(function(value) {
-					return value.name
-				})
-				uni.showActionSheet({
-					itemList: itemList,
-					success: (res) => {
-						uni.share({
-							provider: this.providerList[res.tapIndex].id,
-							scene: this.providerList[res.tapIndex].type && this.providerList[res.tapIndex].type === 'WXSenceTimeline' ?
-								'WXSenceTimeline' : 'WXSceneSession',
-							type: 0,
-							title: 'uni-app模版',
-							summary: e.title,
-							imageUrl: e.img_src,
-							href: 'https://uniapp.dcloud.io',
-							success: (res) => {
-								console.log('success:' + JSON.stringify(res));
-							},
-							fail: (e) => {
-								uni.showModal({
-									content: e.errMsg,
-									showCancel: false
-								})
-							}
-						});
-					}
-				})
 			}
 		}
 	}
 </script>
 
 <style>
-
+	.container {}
+	.swiperBox{
+		height: 360upx;
+		width: 750upx;
+	}
+	.swiper-item img{
+		display: block;
+		object-fit: cover;
+		height: 360upx;
+		width: 750upx;
+	}
+	.swiper{
+		height: 360upx;
+		width: 750upx;
+	}
 </style>
