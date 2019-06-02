4bb8a1166930a5137e6f4a802173d1601712d0c3
MadYoung <1628933578@qq.com>
2019-06-02 21:36:50 +0800

    修改pageJson

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,19 @@
 <template>
-	<view class="container uni-row">
+	<view class="container">
 		<view class="swiperBox">
+			<view class="tipsBox">
+				<view class="tipsIco"></view>
+				<view class="marquee">{{notice}}</view>
+			</view>
 			<view class="uni-padding-wrap">
 				<view class="page-section swiper">
 					<view class="page-section-spacing">
 						<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
 							<swiper-item v-for="(item,index) in advList" :key="index">
-								<view class="swiper-item uni-bg-red">
-									<navigator :url="item.link">
+								<view class="swiper-item">
+									<view @click="toDevLink(item.link)">
 										<img :src="item.image" alt="">
-									</navigator>
+									</view>
 								</view>
 							</swiper-item>
 						</swiper>
@@ -17,18 +21,18 @@
 				</view>
 			</view>
 		</view>
+		<view class="inner_title">
+			<view class="innerT_tl">
+				<view class="jx"></view>
+				<text>直播机构</text>
+				<view class="textBox">持续增加</view>
+			</view>
+			<text class="innerT_tr">共{{total}}个</text>
+		</view>
 		<view class="inner">
 			<view class="inner_item">
-				<view class="inner_title">
-					<view class="innerT_tl">
-						<view class="jx"></view>
-						<text>直播机构</text>
-						<view class="textBox">持续增加</view>
-					</view>
-					<text class="innerT_tr">共{{total}}个</text>
-				</view>
 				<view class="gradBox">
-					<view class="grad_item" v-for="(item,index) in liveList" :key="index">
+					<view class="grad_item" v-for="(item,index) in liveList" :key="index" @click="toLive(item)">
 						<view class="headPic">
 							<img :src="item.img" alt="">
 						</view>
@@ -56,15 +60,16 @@
 				duration: 500,
 				total: 0,
 				advList: [],
-				liveList : []
+				liveList: [],
+				notice: ''
 			}
 		},
 		onLoad() {
 			this.getDev()
 			this.getLive()
 		},
-		methods:{
-			getDev(){
+		methods: {
+			getDev() {
 				uni.request({
 					url: this.$serverUrl + '/mobile/index/index', //仅为示例，并非真实接口地址。
 					method: 'GET',
@@ -72,13 +77,14 @@
 						console.log(res.data)
 						if (res.data.code === 0) {
 							this.advList = res.data.data.advList
+							this.notice = res.data.data.notice
 						} else {
 							console.log(res.data.msg)
 						}
 					}
 				});
 			},
-			getLive(){
+			getLive() {
 				uni.request({
 					url: this.$serverUrl + '/mobile/Live/index', //仅为示例，并非真实接口地址。
 					method: 'POST',
@@ -86,10 +92,19 @@
 						console.log(res.data)
 						if (res.data.code === 0) {
 							this.liveList = res.data.data.lists
+							this.total = res.data.data.count
 						} else {
 							console.log(res.data.msg)
 						}
 					}
+				});
+			},
+			toDevLink(url) {
+				window.location.href=url
+			},
+			toLive(params){
+				uni.navigateTo({
+					url: 'index_detail?title='+params.title+'&name='+params.name+'&img='+params.img
 				});
 			}
 		}
@@ -98,26 +113,65 @@
 
 <style>
 	.container {
+		display: flex;
+		flex-direction: column;
 		background: #fff;
 	}
-	.swiperBox{
+
+	.swiperBox {
+		position: relative;
 		height: 360upx;
 		width: 750upx;
 	}
-	.swiper-item img{
+
+	.tipsBox {
+		position: absolute;
+		z-index: 99;
+		top: 0;
+		left: 0;
+		width: 750upx;
+		height: 50upx;
+		background: rgba(0, 0, 0, 0.5);
+		color: #fff;
+		display: flex;
+		align-items: center;
+		box-sizing: border-box;
+		padding: 0 30upx;
+		font-size: 20upx;
+	}
+
+	.marquee {
+		white-space: nowrap;
+		/* 　　		overflow:-webkit-marquee; */
+	}
+
+	.tipsIco {
+		height: 30upx;
+		width: 30upx;
+		background: url(../../static/icon/laba.png) no-repeat center center;
+		background-size: 100% 100%;
+		margin-right: 10upx;
+	}
+
+	.swiper-item img {
 		display: block;
 		object-fit: cover;
 		height: 360upx;
 		width: 750upx;
 	}
-	.swiper{
+
+	.swiper {
 		height: 360upx;
 		width: 750upx;
 	}
-	.inner{
+
+	.inner {
 		/* width: 750upx; */
+		flex: 1;
+		overflow: auto;
 	}
-	.inner_title{
+
+	.inner_title {
 		box-sizing: border-box;
 		border-bottom: 1px solid #e5e5e5;
 		width: 750upx;
@@ -127,22 +181,27 @@
 		justify-content: space-between;
 		/* background: #007AFF; */
 	}
-	.innerT_tl,.innerT_tr{
-		height: 100%; 
+
+	.innerT_tl,
+	.innerT_tr {
+		height: 100%;
 		display: flex;
 		align-items: center;
 	}
-	.innerT_tl{
+
+	.innerT_tl {
 		font-size: 28upx;
 		color: #333;
 	}
-	.jx{
+
+	.jx {
 		width: 8upx;
 		height: 40upx;
 		background-color: #ccc;
 		margin-right: 10upx;
 	}
-	.textBox{
+
+	.textBox {
 		padding: 5upx 10upx;
 		background-color: #B13531;
 		color: #fff;
@@ -150,47 +209,70 @@
 		margin-left: 10upx;
 		border-radius: 4upx;
 	}
-	.innerT_tr{
+
+	.innerT_tr {
 		font-size: 24upx;
 		color: #666;
 	}
-	.gradBox{
+
+	.gradBox {
 		display: flex;
 		flex-wrap: wrap;
 	}
-	.grad_item{
+
+	.grad_item {
 		width: 33.33%;
 		box-sizing: border-box;
 		padding: 20upx;
-		height: 250upx;
+		height: 230upx;
 		border-right: 1px solid #e5e5e5;
 		border-bottom: 1px solid #e5e5e5;
 		text-align: center;
 	}
-	.grad_item:nth-child(3n){
+
+	.grad_item:nth-child(3n) {
 		border-right: none;
 	}
-	.headPic{
+
+	.headPic {
 		width: 80upx;
 		height: 80upx;
 		background-color: #e5e5e5;
 		margin: 0 auto 20upx;
 	}
-	.headPic img{
+
+	.headPic img {
 		height: 100%;
 		width: 100%;
 		object-fit: cover;
 	}
-	.pName{
+
+	.pName {
 		font-size: 24upx;
 		text-align: center;
 		color: #333;
+		margin-bottom: 10upx;
 	}
-	.playNumBox{
+
+	.playNumBox {
 		display: inline-block;
-		font-size: 24upx;
+		font-size: 20upx;
 		padding: 5upx 10upx;
 		background-color: #B13531;
 		color: #fff;
+		border-radius: 5upx;
+	}
+
+	.playNumInner {
+		display: flex;
+		align-items: center;
+	}
+
+	.playIco {
+		height: 30upx;
+		margin-right: 10upx;
+		width: 30upx;
+		background: url(../../static/icon/video_white.png) no-repeat center center;
+		background-size: 100% 100%;
 	}
 </style>
---
+++ pages/index/index_detail.vue
@@ -0,0 +1,175 @@
+<template>
+	<view class="container">
+		<view class="info">
+			<view class="info_l">
+				<img :src="headPic" alt="">
+			</view>
+			<view class="info_r">
+				<view class="info_rTop">
+					<text class="liveTitle">{{liveTitle}}</text>
+					<text class="ptai">直播平台</text>
+					<view class="numBox">
+						<view class="numBoxIco"></view>
+						<text>{{total}}</text>
+					</view>
+				</view>
+				<view class="info_tips">
+					本平台所有直播及图片内容全部都是由服务器从第三方获取内容
+				</view>
+			</view>
+		</view>
+		<view class="videoList">
+			<view class="vItem" v-for="(item,index) in videoList" :key="index">
+				<video src="item.play_url" :poster="item.img"></video>
+				<view class="pNum">
+					<view class="pIco"></view>
+					<view class="pTotal"></view>
+				</view>
+				<view class="pDes"></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				liveName: '',
+				liveTitle: '',
+				total: 0,
+				videoList: []
+			}
+		},
+		onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数
+			this.liveName = option.name
+			this.liveTitle = option.title
+			this.headPic = option.img
+			console.log(this.headPic)
+			uni.setNavigationBarTitle({
+				title: option.title
+			});
+			this.getLiveHouse()
+		},
+		methods: {
+			getLiveHouse() {
+				uni.request({
+					url: this.$serverUrl + '/mobile/live1/anchors?name='+this.liveName, //仅为示例，并非真实接口地址。
+					method: 'POST',
+					success: (res) => {
+						if (res.data.code === 0) {
+							let data = res.data.data
+							this.total = data.count
+							this.videoList = data.lists
+						} else {
+							console.log(res.data.msg)
+						}
+					}
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+	.container{
+		/* display: flex; */
+		/* min-height: 100%; */
+		width: 750upx;
+		background: #fff;
+		padding: 20upx;
+		box-sizing: border-box;
+	}
+	.info{
+		width: 100%;
+		box-sizing: border-box;
+		padding: 0 20upx;
+		height: 200upx;
+		display: flex;
+		align-items: center;
+		background: url(../../static/icon/banner.png) no-repeat center center;
+		background-size: 100% 100%;
+	}
+	.info_l{
+		height: 120upx;
+		width: 120upx;
+		border-radius: 50%;
+		margin-right: 10upx;
+		overflow: hidden;
+		background: #007AFF;
+	}
+	.info_l img{
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	.info_r{
+		width: 540upx;
+	}
+	.info_rTop{
+		display: flex;
+		/* height: 60upx; */
+		align-items: baseline;
+		font-size: 24upx;
+		border-bottom: 1px solid #fff;
+		color: #fff;
+		margin-bottom: 10upx;
+		padding-bottom: 10upx;
+	}
+	.liveTitle{
+		font-size: 32upx;
+		margin-right: 10upx;
+	}
+	.ptai{
+		margin-right: 10upx;
+	}
+	.info_tips{
+		font-size: 20upx;
+		color: #fff;
+		/* white-space:normal; */
+	}
+	.numBox{
+		display: flex;
+		align-items: center;
+		padding: 4upx 20upx;
+		font-size: 20upx;
+		color: #333;
+		border-radius: 20upx;
+		background-color: #fff;
+	}
+	.numBoxIco{
+		height: 30upx;
+		width: 30upx;
+		background: url(../../static/icon/video_black.png) no-repeat center center;
+		background-size: 100% 100%;
+		margin-right: 10upx;
+	}
+	.videoList{
+		width: 710upx;
+		padding: 20upx 0;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.vItem{
+		position: relative;
+		width: 355upx;
+		height: 280upx;
+		box-sizing: border-box;
+		padding-right: 10upx;
+		margin-bottom: 20upx;
+	}
+	.vItem:nth-child(2n){
+		padding-right: 0upx;
+		padding-left: 10upx;
+	}
+	.vItem video{
+		height: 100%;
+		width: 100%;
+	}
+	.pNum{
+		position: absolute;
+	}
+	.pDes{
+		position: absolute;
+	}
+</style>
